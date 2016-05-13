aa90d25307ea16f4ea681b0c392ce89a996d3deb
vallejoco <vallejoco@univ-brest.fr>
2016-05-13 17:57:02 +0200

    Add: MinimizeToUpdate MinimizeStrong

--- ModifRoundtrip/ModifMetamodels/src/modif/util/Minimize.java
+++ ModifRoundtrip/ModifMetamodels/src/modif/util/Minimize.java
@@ -11,15 +11,31 @@
  */
 package modif.util;
 
+import java.util.ArrayList;
+
 import modif.*;
 
 public class Minimize {
-	
+
+	private int minimizeType;
+
 	public void go(Modifications modif) {
-						
+
 		go(modif.getRootPackageModification());
 	}
-	
+
+	/**
+	 * Minimize
+	 * 
+	 * @param modif Modif specification to be simplified.
+	 * @param type Type of minimize to be applied. 
+	 * 1 = toUpdate.  2 = strong.
+	 */
+	public void go(Modifications modif, int type) {
+		minimizeType = type;
+		go(modif.getRootPackageModification());
+	}
+
 	private void go(PackageModification modif) {
 		for (PackageModification subPackage : modif.getPackageModification()) {
 			go(subPackage);
@@ -34,34 +50,34 @@ public class Minimize {
 			go(eNum);
 		}
 	}
-	
+
 	private void go(ClassModification subClass) {
-		
+
 		if (subClass.isRemove() || subClass.isHide()) {
 			subClass.setNewName("");
 			//subClass.setNewName(null);
-			
+
 			if (subClass.isRemove() ) {
 				subClass.getFeatureModification().clear();
 			}
-			
+
 			subClass.getAddFeatures().clear();
 			subClass.getAddDeriveds().clear();
 			subClass.getAnnotationModification().clear();
-			
+
 			subClass.setRemoveEAnnotations(false);
-			
+
 			subClass.setChangeAbstract(false);
-			
+
 			if (subClass.isRemove()) {
 				subClass.setHide(false);			
 				subClass.setFlatten(false);
 				subClass.setFlattenAll(false);
 			}
 			subClass.setEnumerate(null);
-			
+
 		} else {
-			
+
 			if (subClass.getOldName().equals(subClass.getNewName())) {
 				//subClass.setNewName(null);
 				subClass.setNewName("");
@@ -75,30 +91,45 @@ public class Minimize {
 				}
 			}
 		}
-		
+
 	}
-	
-	private void go(AttributeModification am) {
-		
+
+	private ArrayList<String> go(AttributeModification am) {
+		ArrayList<String> unchangedAtt = new ArrayList<String>();
+		boolean sameName = false;
+		boolean sameBounds = false;
+
 		if (am.isRemove()) {
-			//am.setNewName(null);
 			am.setNewName("");
 			am.getAnnotationModification().clear();
 			am.setRemoveEAnnotations(false);	
 			am.setChangeType(false);
-			
+			am.setChangeBounds(null);
+
 		} else {
 			if (am.getOldName().equals(am.getNewName())) {
-				//am.setNewName(null);
+				am.setNewName(null);
 				am.setNewName("");
 			}
+			if(am.getChangeBounds().getOldLower() == am.getChangeBounds().getNewLower() 
+					&& am.getChangeBounds().getOldUpper() == am.getChangeBounds().getNewUpper()){
+				if(minimizeType == 1) {
+
+				}else {
+					sameBounds = true;
+					am.setChangeBounds(null);
+				}
+			}
+			if(sameName && sameBounds) {
+				unchangedAtt.add(am.getOldName());
+			}
 		}
+		return unchangedAtt;
 	}
-	
+
 	private void go(ReferenceModification fm) {
-		
+
 		if (fm.isRemove()) {
-			//fm.setNewName(null);
 			fm.setNewName("");
 			fm.getAnnotationModification().clear();
 			fm.setRemoveEAnnotations(false);	
@@ -106,19 +137,25 @@ public class Minimize {
 			fm.setRemoveOpposite(false);
 			fm.setAddOpposite(null);
 			fm.setReify(null);
-			
+
 		} else {
 			if (fm.getOldName().equals(fm.getNewName())) {
-				//fm.setNewName(null);
 				fm.setNewName("");
+			}
+			if(fm.getChangeBounds().getOldLower() == fm.getChangeBounds().getNewLower() 
+					&& fm.getChangeBounds().getOldUpper() == fm.getChangeBounds().getNewUpper()){
+				if(minimizeType == 1) {
+
+				}else{
+					fm.setChangeBounds(null);
+				}
 			}
 		}
 	}
-	
+
 	private void go(DataTypeModification edm) {
-		
+
 		if (edm.isRemove()) {
-			//edm.setNewName(null);
 			edm.setNewName("");
 			edm.setOldInstanceTypeName(null);
 			edm.setNewInstanceTypeName(null);
@@ -126,14 +163,13 @@ public class Minimize {
 			edm.setRemoveEAnnotations(false);
 		} else {
 			if (edm.getOldName().equals(edm.getNewName())) {
-				//edm.setNewName(null);
 				edm.setNewName("");
 			}
 		}
 	}
-	
+
 	private void go(EnumModification enm) {
-		
+
 		if (enm.isRemove()) {
 			//enm.setNewName(null);
 			enm.setNewName("");
@@ -152,5 +188,5 @@ public class Minimize {
 			}
 		}
 	}
-	
+
 }
