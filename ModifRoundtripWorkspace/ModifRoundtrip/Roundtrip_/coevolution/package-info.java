b55bd846c495e2190b87d1a4785c618e1c44f2f8
vallejoco <vallejoco@univ-brest.fr>
2016-05-25 17:09:03 +0200

    Rename: workspace

--- ModifRoundtripWorkspace/ModifRoundtrip/Roundtrip_/coevolution/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * This package gathers classes and utilitary functions used to perform
- * a complete migration roundtrip in order to reuse a rewriting tool.
- * @author M. Kerboeuf
- */
-package coevolution;
